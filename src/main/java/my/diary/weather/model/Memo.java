5fac0d9e24c67c9042ada5cebd35dd3d528903e1
Park, Su Eun <park_sueun@naver.com>
2026-02-13 23:11:18 +0800

    create memo model & repository

---
+++ src/main/java/my/diary/weather/model/Memo.java
@@ -0,0 +1,15 @@
+package my.diary.weather.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Memo {
+    private int id;
+    private String text;
+}
