b165ef624c1aec0600b029947870de632f014a60
Park, Su Eun <park_sueun@naver.com>
2026-02-18 21:55:09 +0800

    add query methods for Diary by user and diaryDate

--- src/main/java/my/diary/weather/repository/DiaryRepository.java
+++ src/main/java/my/diary/weather/repository/DiaryRepository.java
@@ -1,9 +1,13 @@
 package my.diary.weather.repository;
-
 import my.diary.weather.model.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Repository
-public interface DiaryRepository extends JpaRepository<Diary, Integer> {
+public interface DiaryRepository extends JpaRepository<Diary, Long> {
+    boolean existsByUser_IdAndDiaryDate(Long userId, LocalDate date);
+    Optional<Diary> findByUser_IdAndDiaryDate(Long userId, LocalDate date);
 }
